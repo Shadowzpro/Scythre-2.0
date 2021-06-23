3d424aa7cbf8fb0cf7d4837c7ae12b6bd9400c6d
Jake <jake.yveltal@gmail.com>
2021-06-23 23:59:10 +1000

    Added a Lot
    Probs broke something idk ask me tomorrow

---
+++ Scythre/Assets/PlayerMovement.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform cam;
+    public float speed = 12;
+    public CharacterController controller;
+    public float smoothTime = 0.1f;
+    float turnSmoothly;
+    public LayerMask layermask;
+    public GameObject[] linecastPoints;
+    RaycastHit hit;
+
+    [HideInInspector]
+    public bool canMove = true;
+    Vector3 moveDirection = Vector3.zero;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    float CayoteeTime = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 20;
+        }
+        else
+        {
+            speed = 12;
+        }
+        Vector3 direction;
+        float movementDirectionY = moveDirection.y;
+
+        if (Input.GetButton("Jump")/* && (canMove && controller.isGrounded)*/ && CayoteeTime <= 0.5f)
+        {
+            moveDirection.y = jumpSpeed;
+            CayoteeTime = 1.0f;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (controller.isGrounded)
+        {
+            CayoteeTime = 0.0f;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!controller.isGrounded)
+        {
+            if (CayoteeTime <= 0.5f)
+            {
+                CayoteeTime += Time.deltaTime;
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        direction = new Vector3(x, 0f, z).normalized;
+
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAng = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAng, ref turnSmoothly, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAng, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+
+    }
+
+}
+
+
--- Scythre/Assets/Scripts/Player_Movement.cs
+++ Scythre/Assets/Scripts/Player_Movement.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    public float m_speed = 5f;
-    public float rotateSpeed = 20f;
-    public Vector3 m_Input;
+    public CharacterController controller;
+    public float m_speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+  //  public float rotateSpeed = 20f;
+
+
     public GameObject playerMesh;
-    public GameObject mainCamera;
+    public Transform mainCamera;
     public float cameraYpos;
     public bool isDead;
     
@@ -17,30 +22,41 @@ public class Player_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        controller = playerMesh.GetComponent<CharacterController>();
         isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float cameraXpos = this.transform.position.x - 3;
-        float cameraZpos = this.transform.position.z;
-        mainCamera.transform.position = new Vector3(cameraXpos, cameraYpos, cameraZpos);
+      //  float cameraXpos = this.transform.position.x - 3;
+      //  float cameraZpos = this.transform.position.z;
+      //  mainCamera.transform.position = new Vector3(cameraXpos, cameraYpos, cameraZpos);
     }
 
     void FixedUpdate()
     {
         if (isDead == false)
-        { 
-            var rotationVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            var rotation = Quaternion.LookRotation(rotationVector);
-            m_Input = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
-            m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * m_speed);
-            if ((rotationVector.x != 0) || (rotationVector.z != 0))
+        {
+            //  var rotationVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //  var rotation = Quaternion.LookRotation(rotationVector);
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+           Vector3 m_Input = new Vector3(horizontal, 0, vertical).normalized;
+            if (m_Input.magnitude >= 0.1f)
             {
-                playerMesh.gameObject.transform.rotation = Quaternion.RotateTowards(playerMesh.gameObject.transform.rotation, rotation, rotateSpeed);
+                float targetAngle = Mathf.Atan2(m_Input.x, m_Input.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                playerMesh.gameObject.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 move_Dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(move_Dir.normalized * Time.deltaTime * m_speed);
             }
+        //    if ((rotationVector.x != 0) || (rotationVector.z != 0))
+      //      {
+       //         playerMesh.gameObject.transform.rotation = Quaternion.RotateTowards(playerMesh.gameObject.transform.rotation, rotation, rotateSpeed);
+       //     }
         }
     }
 }
